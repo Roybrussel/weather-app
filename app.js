9989558e4e99295ed352005a60cf2763353b84bf
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-01-15 13:28:03 +0100

    Searching for a city is working.

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const https = require("https");
-const { CLIENT_RENEG_LIMIT } = require("tls");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
-  const apiUrl =
-    "https://api.openweathermap.org/data/2.5/forecast?q=amsterdam&appid=2ce2148802413615284b6746ea7e6b25&units=metric";
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const city = req.body.cityName;
+  const apiKey = "2ce2148802413615284b6746ea7e6b25";
+  const unit = "metric";
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${unit}`;
   https.get(apiUrl, function (response) {
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
@@ -14,13 +23,21 @@ app.get("/", function (req, res) {
       const weatherDescription = weatherData.list[0].weather[0].description;
       const icon = weatherData.list[0].weather[0].icon;
       res.write(
-        `<h1>The current temperature in Amsterdam is ${temp} (celcius)</h1>`
+        `<h1>The current temperature in ${city} is ${temp} (celcius)</h1>`
       );
       res.write(`Weather description: ${weatherDescription}`);
       res.write(
         `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="weather-icon"/>`
       );
       res.send();
+
+      const test = {
+        name: "test",
+        lastname: "test",
+        middlename: "test",
+      };
+
+      console.log(test);
     });
   });
 });
